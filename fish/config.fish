a3ef66966466e40a1c6fc24be7fe6c599a2d95f5
Alejandro.DGZ <3NJDEGUZMAN@gmail.com>
2025-07-06 18:59:57 +0100

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+#source /opt/homebrew/share/z/z.fish
+eval "$(/opt/homebrew/bin/brew shellenv)"
---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,39 @@
+return {
+  { "EdenEast/nightfox.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "carbonfox",
+    },
+  },
+}
+
+-- return {
+--   { "bettervim/yugen.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "yugen",
+--     },
+--   },
+-- }
+
+-- return {
+--   { "DonJulve/NeoCyberVim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "neocybervim",
+--     },
+--   },
+-- }
+
+-- return {
+--   { "rebelot/kanagawa.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "kanagawa",
+--     },
+--   },
+-- }
